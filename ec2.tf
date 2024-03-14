aa6b5f504fb1ddd2585a6d82611f94fd612fea3e
sid <siddhu010598@gmail.com>
2024-03-14 09:10:15 +0530

    added statelocking

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "sg" {
-  name = var.sg_name
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "ALL"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-}
--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "mybucket14032024"
     key = "terraform.tfstate"
     region = "ap-southeast-1"
-    
+    dynamodb_table = "statelocking"
   }
 }
 provider "aws" {
